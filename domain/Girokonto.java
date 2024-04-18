c55a4b5cdb4a2c315b75b1bbc34d9bb70e4686b8
Andreas Bulai <2111063@stud.hs-mannheim.de>
2024-04-18 19:09:40 +0200

    Klassen Sparkonto und Girokonto angelegt und Konstrukor für beide Klassen erstellt

---
+++ domain/Girokonto.java
@@ -0,0 +1,8 @@
+package domain;
+
+public class Girokonto extends Konto{
+
+    public Girokonto(String inhaber, int kontonummer, double betrag) {
+        super(inhaber, kontonummer, betrag);
+    }
+}
---
+++ domain/Sparkonto.java
@@ -0,0 +1,7 @@
+package domain;
+
+public class Sparkonto extends Konto{
+    public Sparkonto(String inhaber, int kontonummer, double betrag) {
+        super(inhaber, kontonummer, betrag);
+    }
+}
