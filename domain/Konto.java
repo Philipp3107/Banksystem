8b4ba8e42c114fc26a74d23fb3cdab2bd432ea10
Andreas Bulai <2111063@stud.hs-mannheim.de>
2024-04-18 18:55:44 +0200

    Anlegen und Initialisieren der Atribute, sowie getter und setter Methoden implementiert

--- domain/Konto.java
+++ domain/Konto.java
@@ -1,1 +1,30 @@
-//Kontoklasse
+package domain;
+public abstract class Konto{
+    private static String inhaber;
+    private static int kontonummer;
+    private double betrag;
+
+    public static String getInhaber() {
+        return inhaber;
+    }
+
+    public static void setInhaber(String inhaber) {
+        Konto.inhaber = inhaber;
+    }
+
+    public static int getKontonummer() {
+        return kontonummer;
+    }
+
+    public static void setKontonummer(int kontonummer) {
+        Konto.kontonummer = kontonummer;
+    }
+
+    public double getBetrag() {
+        return betrag;
+    }
+
+    public void setBetrag(double betrag) {
+        this.betrag = betrag;
+    }
+}
